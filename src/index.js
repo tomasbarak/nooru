8ffaa6d4f4c70fffff13ad915723c63e88880727
tomasbarak <barakcinthia@gmail.com>
2022-09-26 08:56:10 -0300

    SPOTIFY LOGINNNNNN

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ const { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } = require(
 const oauth2 = require('simple-oauth2');
 const path = require('path');
 const crypto = require('crypto');
+const axios = require('axios');
+
 const credentials = {
     client: {
         id: process.env.spotify_client_id, // Change this!
         secret: process.env.spotify_client_secret, // Change this!
     },
     auth: {
-        tokenHost: 'https://api.spotify.com',
-        tokenPath: '/oauth/access_token'
+        tokenHost: 'https://accounts.spotify.com',
+        tokenPath: '/authorize'
     }
 };
 
@@ -32,27 +34,52 @@ app.get('/', function (req, res) {
     res.render(__dirname + '/public/html/index.ejs');
 });
 
-app.all('/spotify-callback', (req, res) => {
-    console.log(req);
+app.all('/spotify-redirect', (req, res) => {
+    if (!req.cookies.state) {
+        res.status(400).send('State cookie not set or expired. Maybe you took too long to authorize. Please try again.');
+        // Check the State Cookie is equal to the state parameter.
+    } else if (req.cookies.state !== req.query.state) {
+        res.status(400).send('State validation failed');
+    }
+
+    let body = {
+        grant_type: 'authorization_code',
+        code: req.query.code,
+        redirect_uri: `${req.protocol}://${req.hostname}:3000/spotify-redirect`,
+        client_id: process.env.spotify_client_id,
+        client_secret: process.env.spotify_client_secret
+    }
+
+    const encoded = `?grant_type=authorization_code&code=${req.query.code}&redirect_uri=${encodeURIComponent(`${req.protocol}://${req.hostname}:3000/spotify-redirect`)}&client_id=${process.env.spotify_client_id}&client_secret=${process.env.spotify_client_secret}`
+
+    axios.post('https://accounts.spotify.com/api/token'+encoded).then((response) => {
+        console.log(response.data)
+        axios.get('https://api.spotify.com/v1/me', {
+            headers: {
+                'Authorization': 'Bearer ' + response.data.access_token
+            }
+        }).then((response) => {
+            console.log(response.data)
+            res.send(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }).catch((error) => {
+        console.log(error)
+        res.send(error)
+    });
 });
 
 app.listen(port, function () {
     console.log('Listening on port ' + port);
 
-    
+
 });
 
 
 app.get('/redirect', (req, res) => {
-    // Generate a random state verification cookie.
     const state = req.cookies.state || crypto.randomBytes(20).toString('hex');
-    // Allow unsecure cookies on localhost.
     const secureCookie = req.get('host').indexOf('localhost:') !== 0;
-    res.cookie('state', state.toString(), {maxAge: 3600000, secure: secureCookie, httpOnly: true});
-    const redirectUri = client.authorizeURL({
-      redirect_uri: `${req.protocol}://${req.get('host')}/spotify-callback`,
-      scope: 'basic',
-      state: state
-    });
-    res.redirect(redirectUri);
-  });
+    res.cookie('state', state.toString(), { maxAge: 3600000, secure: secureCookie, httpOnly: true });
+    res.redirect(`https://accounts.spotify.com/authorize?client_id=${process.env.spotify_client_id}&response_type=code&redirect_uri=${req.protocol}://${req.get('host')}/spotify-redirect&scope=user-read-private%20user-read-email&state=${state}`);
+});
